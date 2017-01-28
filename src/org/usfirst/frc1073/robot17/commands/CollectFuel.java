a231815a6f31a468d5f12a7be81fb63c2042f296
Zac Schofield <zacmail@gmail.com>
2017-01-28 10:22:47 -0500

    Created collectFuel command

--- src/org/usfirst/frc1073/robot17/commands/CollectFuel.java
+++ src/org/usfirst/frc1073/robot17/commands/CollectFuel.java
@@ -41,6 +41,9 @@ public class CollectFuel extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	double collectorSpeed = 0.5;
+    	
+    	Robot.collector.collectorSpin(collectorSpeed);
     }
 
     // Make this return true when this Command no longer needs to run execute()
