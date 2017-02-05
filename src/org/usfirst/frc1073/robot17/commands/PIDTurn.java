327b71c30d83b70a79e6fafeb6bcc00527c7e24e
Vijay <vijay.chilakapati@gmail.com>
2017-02-05 13:18:30 -0500

    Now Robot can turn 50 degrees
    TeleOp works along with Smartdashboard PID command to turn 50 degrees

--- src/org/usfirst/frc1073/robot17/commands/PIDTurn.java
+++ src/org/usfirst/frc1073/robot17/commands/PIDTurn.java
@@ -39,13 +39,19 @@ public class PIDTurn extends Command {
     }
     double error;
     double angle;
+    static double c = 0.03;
+    double p, i, d;
 
 
     // Called just before this Command runs the first time
     protected void initialize() {
     	RobotMap.pIDDrivegyro.reset();
     	Robot.pIDDrive.getPIDController().reset();
-    	Robot.pIDDrive.setSetpoint(90);
+    	Robot.pIDDrive.setSetpoint(-50); // 50 degrees for now
+//    	SmartDashboard.getNumber("P", p);
+//    	SmartDashboard.getNumber("I", i);
+//    	SmartDashboard.getNumber("D", d);
+//    	Robot.pIDDrive.getPIDController().setPID(p, i, d);
     	Robot.pIDDrive.enable();
     	Robot.pIDDrive.setAbsoluteTolerance(10);
     	
@@ -54,19 +60,26 @@ public class PIDTurn extends Command {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	error = Robot.pIDDrive.getPIDController().getError();
-    	//SmartDashboard.putNumber("Error",error );
+    	SmartDashboard.putNumber("Error",error );
     	angle = RobotMap.pIDDrivegyro.getAngle();
-    	//SmartDashboard.putNumber("Angle",angle );
-
+    	SmartDashboard.putNumber("Angle",angle );
+    	RobotMap.pIDDriveleftMotor1.set(-0.3);
+    	RobotMap.pIDDriverightMotor1.set(0.3);
+    	//Robot.pIDDrive.basicDrive(-0.3, 0.3);
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+    	if(Math.abs(Robot.pIDDrive.getPIDController().getError() ) < 5)
+    		return true;
+    	else
+    		return false;
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.pIDDrive.disable();
+    	
     }
 
     // Called when another command which requires one or more of the same
