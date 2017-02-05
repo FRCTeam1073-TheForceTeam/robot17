e3f93ad1fb8e2b9285a883cab319bbf9d3cfb7cb
Maxitothegreat <maxitothegreat@gmail.com>
2017-02-05 13:36:52 -0500

    added a drive command without cubic scale or deadzone
    for use in commands where you are not using the joysticks as a analog
    input

--- src/org/usfirst/frc1073/robot17/commands/forward.java
+++ src/org/usfirst/frc1073/robot17/commands/forward.java
@@ -55,11 +55,11 @@ public class forward extends Command {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	if(Math.round(RobotMap.sensorsgyro.getAngle()) <= startAngle-5)
-    		Robot.driveTrain.basicDrive(correctionSpeed, speed);
+    		Robot.driveTrain.basicAutoDrive(correctionSpeed, speed);
     	else if(Math.round(RobotMap.sensorsgyro.getAngle()) >= startAngle+5)
-    		Robot.driveTrain.basicDrive(speed, correctionSpeed);
+    		Robot.driveTrain.basicAutoDrive(speed, correctionSpeed);
     	else 
-    		Robot.driveTrain.basicDrive(speed, speed);
+    		Robot.driveTrain.basicAutoDrive(speed, speed);
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -70,12 +70,12 @@ public class forward extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.driveTrain.basicDrive(0, 0);
+    	Robot.driveTrain.basicAutoDrive(0, 0);
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-    	Robot.driveTrain.basicDrive(0, 0);
+    	Robot.driveTrain.basicAutoDrive(0, 0);
     }
 }
--- src/org/usfirst/frc1073/robot17/commands/turn.java
+++ src/org/usfirst/frc1073/robot17/commands/turn.java
@@ -45,7 +45,7 @@ public class turn extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.driveTrain.basicDrive(Robot.sensors.turnAuto(m_degrees), -Robot.sensors.turnAuto(m_degrees));
+    	Robot.driveTrain.basicAutoDrive(Robot.sensors.turnAuto(m_degrees), -Robot.sensors.turnAuto(m_degrees));
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -56,12 +56,12 @@ public class turn extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.driveTrain.basicDrive(0, 0);
+    	Robot.driveTrain.basicAutoDrive(0, 0);
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-    	Robot.driveTrain.basicDrive(0, 0);
+    	Robot.driveTrain.basicAutoDrive(0, 0);
     }
 }
