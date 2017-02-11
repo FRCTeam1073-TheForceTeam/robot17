b2246fb19cb074554633ad02fa758780fea7694b
Jack McEvoy <jackmcevoy15@gmail.com>
2017-02-11 14:13:28 -0500

    Revert "Added autonomous chooser, added more autonomous modes"
    This reverts commit 7e8f9df802aab713decdc32bee4e4ae53dd7ce98.

--- src/org/usfirst/frc1073/robot17/commands/AutonomousGearFuelHighCross.java
+++ src/org/usfirst/frc1073/robot17/commands/AutonomousGearFuelHighCross.java
@@ -47,14 +47,9 @@ public class AutonomousGearFuelHighCross extends CommandGroup {
     	/**Drive and drop off gear
     	 * TODO add values
     	 **/
-    	addSequential(new AutoDrive(.5, 100));
+    	addSequential(new AutoDrive(0, 0));
     	addSequential(new DriveToGearPeg());
-    	try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    	
     	/**Drive to Boiler
     	 * TODO add values
     	 **/
