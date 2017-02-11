828ef4e335a0f85b301f544cc13be90e55368afe
sreekarc <sreekarchilakapati@gmail.com>
2017-02-11 14:16:46 -0500

    Logger that works
    To use you have to put Logger.setLog();

--- src/org/usfirst/frc1073/robot17/Logger.java
+++ src/org/usfirst/frc1073/robot17/Logger.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc1073.robot17;
 
+
 import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -10,25 +11,25 @@ import java.util.Calendar;
 
 public class Logger {
 	
-	public static void setLog(String content) {
-
-		String timeStamp = new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime());
-		String nameOfClass = Thread.currentThread().getStackTrace()[2].getClassName();
-		
-		try(FileWriter fw = new FileWriter("Logger.txt", true);
-			    BufferedWriter bw = new BufferedWriter(fw);
-			    PrintWriter out = new PrintWriter(bw))
-			{
-			    out.println(timeStamp + "::" + nameOfClass + ": " + content + "\n");
-			} catch (IOException e) {}
+	//Constructor
+	// open file
+	public Logger(){
 		
 	}
-	public static void clear(String FileName){
-		PrintWriter writer;
-		try {
-			writer = new PrintWriter(FileName);
-			writer.print("");
-			writer.close();
-		} catch (FileNotFoundException e) {e.printStackTrace();}
+	
+// just output user string with time and function name
+	public void setLog(String content) {
+
+		String timeStamp = new SimpleDateFormat("HH:mm:ss:ms").format(Calendar.getInstance().getTime());
+		String nameOfClass = Thread.currentThread().getStackTrace()[2].getClassName();
+		String nameOfFunction = Thread.currentThread().getStackTrace()[2].getMethodName();
+		
+		try(FileWriter fw = new FileWriter("/home/lvuser/Logger.txt", true)){
+			    BufferedWriter bw = new BufferedWriter(fw);
+			    PrintWriter out = new PrintWriter(bw);
+			    fw.write(timeStamp + "::" + nameOfClass + "." + nameOfFunction + ": " + content + "\n");
+			    fw.close();
+			} catch (IOException e) {}
+		
 	}
 }
