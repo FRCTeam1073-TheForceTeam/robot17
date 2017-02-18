f22afd397590a16bacb426fc09a74134f2d88844
Maxitothegreat <maxitothegreat@gmail.com>
2017-02-18 10:13:00 -0500

    safety net for geartopeg

--- src/org/usfirst/frc1073/robot17/commands/DriveToGearPeg.java
+++ src/org/usfirst/frc1073/robot17/commands/DriveToGearPeg.java
@@ -142,10 +142,12 @@ public class DriveToGearPeg extends Command {
     }
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.driveTrain.basicDrive(0, 0);
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	Robot.driveTrain.basicDrive(0, 0);
     }
 }
