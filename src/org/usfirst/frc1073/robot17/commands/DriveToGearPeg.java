945df813cfab934666aec053dc75599c44fc074b
bugeybot <bugeybot@gmail.com>
2017-02-18 12:17:09 -0500

    Revert "safety net for geartopeg"
    This reverts commit f22afd397590a16bacb426fc09a74134f2d88844.

--- src/org/usfirst/frc1073/robot17/commands/DriveToGearPeg.java
+++ src/org/usfirst/frc1073/robot17/commands/DriveToGearPeg.java
@@ -142,12 +142,10 @@ public class DriveToGearPeg extends Command {
     }
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.driveTrain.basicDrive(0, 0);
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-    	Robot.driveTrain.basicDrive(0, 0);
     }
 }
