77604780a4ffaf5a17c3b95c57ae78181915a61e
Xandeh <xanderhopkins@hotmail.com>
2017-02-18 14:44:03 -0500

    Put bling support for some commands

--- src/org/usfirst/frc1073/robot17/commands/LaunchAlign.java
+++ src/org/usfirst/frc1073/robot17/commands/LaunchAlign.java
@@ -37,6 +37,7 @@ public class LaunchAlign extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	Robot.bling.sendBoilerTargeting();
     }
 
     // Called repeatedly when this Command is scheduled to run
