430dd978c0481e7dad2bc204107ac9cb33159654
Tristan Hoenninger <tristan.edward.hoenninger@gmail.com>
2017-04-23 13:36:05 -0400

    Logger Changes
    Added a chooser on the SmartDashboard to select match type. Logger will
    now make seperate files for competitive and practice matches.

--- src/org/usfirst/frc1073/robot17/Logger.java
+++ src/org/usfirst/frc1073/robot17/Logger.java
@@ -19,18 +19,42 @@ public class Logger {
 	
 // just output user string with time and function name
 	public static void setLog(String content) {
-		String matchortime = Robot.robotPreferences.getString("match", Robot.starttime);
-		String timeStamp = new SimpleDateFormat("HH:mm:ss:ms").format(Calendar.getInstance().getTime());
-		String nameOfClass = Thread.currentThread().getStackTrace()[2].getClassName();
-		String nameOfFunction = Thread.currentThread().getStackTrace()[2].getMethodName();
-		String fileName = "/home/lvuser/Logger" + matchortime + ".txt";
-		
-		try(FileWriter fw = new FileWriter(fileName, true)){
-			    BufferedWriter bw = new BufferedWriter(fw);
-			    PrintWriter out = new PrintWriter(bw);
-			    fw.write(timeStamp + "::" + nameOfClass + "." + nameOfFunction + ": " + content + "\n");
-			    fw.close();
-			} catch (IOException e) {}
-		
+		Boolean log = true;
+		String matchType = "Error";
+		int matchNumber = 0;
+		//String match = Robot.robotPreferences.getString("match", Robot.starttime);
+		if(OI.matchType == "Practice")
+		{
+			log = true;
+			matchType = "Practice";
+			//matchNumber = OI.practiceMatch;
+		}
+		else if (OI.matchType == "Competitive")
+		{
+			log = true;
+			matchType = "Competitive";
+			//matchNumber = OI.competitiveMatch;
+		}
+		else
+		{
+			log = false;
+		}
+		if (log == true)
+		{
+			String comp = Robot.robotPreferences.getString("Competition", "Worlds2017");
+			String timeStamp = new SimpleDateFormat("HH:mm:ss:ms").format(Calendar.getInstance().getTime());
+			String timeStamp1 = new SimpleDateFormat("HH:mm").format(Calendar.getInstance().getTime());
+			String nameOfClass = Thread.currentThread().getStackTrace()[2].getClassName();
+			String nameOfFunction = Thread.currentThread().getStackTrace()[2].getMethodName();
+			String fileName = "/home/lvuser/Logger"+ timeStamp1 + comp + matchType + ".txt";
+			
+			try(FileWriter fw = new FileWriter(fileName, true)){
+				    BufferedWriter bw = new BufferedWriter(fw);
+				    PrintWriter out = new PrintWriter(bw);
+				    fw.write(timeStamp + "::" + nameOfClass + "." + nameOfFunction + ": " + content + "\n");
+				    fw.close();
+				} catch (IOException e) {}
+			}
+				
 	}	
 }
